03db232d0207f4d25704de91a99d9d272fc95cd5
Brian Elder <gth747m@gmail.com>
2021-01-01 16:41:19 -0500

    Added CMake, removed googletest.

---
+++ PointProcessorLib/CMakeFiles/PointProcessorLib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/Frequency.cpp" "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/CMakeFiles/PointProcessorLib.dir/Frequency.cpp.o"
+  "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/NamedMutex.cpp" "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/CMakeFiles/PointProcessorLib.dir/NamedMutex.cpp.o"
+  "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/Quality.cpp" "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/CMakeFiles/PointProcessorLib.dir/Quality.cpp.o"
+  "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/SharedMemory.cpp" "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/CMakeFiles/PointProcessorLib.dir/SharedMemory.cpp.o"
+  "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/dllmain.cpp" "/home/brelder/code/cpp/PointProcessor/PointProcessorLib/CMakeFiles/PointProcessorLib.dir/dllmain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PointProcessorLib/CMakeFiles/PointProcessorLib.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../lib/libPointProcessorLib.a"
+  "../lib/libPointProcessorLib.pdb"
+  "CMakeFiles/PointProcessorLib.dir/Frequency.cpp.o"
+  "CMakeFiles/PointProcessorLib.dir/NamedMutex.cpp.o"
+  "CMakeFiles/PointProcessorLib.dir/Quality.cpp.o"
+  "CMakeFiles/PointProcessorLib.dir/SharedMemory.cpp.o"
+  "CMakeFiles/PointProcessorLib.dir/dllmain.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointProcessorLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
