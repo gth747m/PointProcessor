3e05cbf456e74052d72999de96836dc36183f3b1
Brian Elder <gth747m@gmail.com>
2021-01-12 19:29:10 -0500

    Updated Sum and Avg tests.

---
+++ PointProcessorTest/AvgeragePointTest.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+
+using namespace PointProcessor;
+
+TEST(AveragePoint, AverageInt32)
+{
+    auto inpA = Point<int32_t>();
+    auto inpB = Point<int32_t>();
+    auto inpC = Point<int32_t>();
+    auto avg = AveragePoint<int32_t>();
+    avg.add_input(&inpA);
+    avg.add_input(&inpB);
+    avg.add_input(&inpC);
+    inpA.set_value(2);
+    inpB.set_value(4);
+    inpC.set_value(3);
+    avg.calculate();
+    ASSERT_EQ(avg.get_value(), 3);
+}
+
+TEST(AveragePoint, AverageInt64)
+{
+    auto inpA = Point<int64_t>();
+    auto inpB = Point<int64_t>();
+    auto inpC = Point<int64_t>();
+    auto avg = AveragePoint<int64_t>();
+    avg.add_input(&inpA);
+    avg.add_input(&inpB);
+    avg.add_input(&inpC);
+    inpA.set_value(25);
+    inpB.set_value(45);
+    inpC.set_value(20);
+    avg.calculate();
+    ASSERT_EQ(avg.get_value(), 30);
+}
+
+TEST(AveragePoint, AverageFloat)
+{
+    auto inpA = Point<float>();
+    auto inpB = Point<float>();
+    auto inpC = Point<float>();
+    auto avg = AveragePoint<float>();
+    avg.add_input(&inpA);
+    avg.add_input(&inpB);
+    avg.add_input(&inpC);
+    inpA.set_value(2.5);
+    inpB.set_value(9.5);
+    avg.calculate();
+    ASSERT_EQ(avg.get_value(), 4.0);
+}
+
+TEST(AveragePoint, AverageDouble)
+{
+    auto inpA = Point<double>();
+    auto inpB = Point<double>();
+    auto avg = AveragePoint<double>();
+    avg.add_input(&inpA);
+    avg.add_input(&inpB);
+    inpA.set_value(25.25);
+    inpB.set_value(45.25);
+    avg.calculate();
+    ASSERT_EQ(avg.get_value(), 35.25);
+}
--- PointProcessorTest/SumPointTest.cpp
+++ PointProcessorTest/SumPointTest.cpp
@@ -2,12 +2,33 @@
 
 using namespace PointProcessor;
 
-TEST(SumPoint, SumInt)
+TEST(SumPoint, SumInt32)
 {
-    Point<int> inpA = Point<int>();
-    Point<int> inpB = Point<int>();
-    Point<int> inpC = Point<int>();
-    SumPoint<int> sum = SumPoint<int>();
+    auto inpA = Point<int32_t>();
+    auto inpB = Point<int32_t>();
+    auto inpC = Point<int32_t>();
+    auto sum = SumPoint<int32_t>();
+    sum.add_input(&inpA);
+    sum.add_input(&inpB);
+    sum.add_input(&inpC);
+    inpA.set_value(1);
+    inpB.set_value(2);
+    inpC.set_value(3);
+    sum.calculate();
+    ASSERT_EQ(sum.get_value(), 6);
+    inpA.set_value(4);
+    inpB.set_value(5);
+    inpC.set_value(6);
+    sum.calculate();
+    ASSERT_EQ(sum.get_value(), 15);
+}
+
+TEST(SumPoint, SumInt64)
+{
+    auto inpA = Point<int64_t>();
+    auto inpB = Point<int64_t>();
+    auto inpC = Point<int64_t>();
+    auto sum = SumPoint<int64_t>();
     sum.add_input(&inpA);
     sum.add_input(&inpB);
     sum.add_input(&inpC);
@@ -25,10 +46,31 @@ TEST(SumPoint, SumInt)
 
 TEST(SumPoint, SumFloat)
 {
-    Point<float> inpA = Point<float>();
-    Point<float> inpB = Point<float>();
-    Point<float> inpC = Point<float>();
-    SumPoint<float> sum = SumPoint<float>();
+    auto inpA = Point<float>();
+    auto inpB = Point<float>();
+    auto inpC = Point<float>();
+    auto sum = SumPoint<float>();
+    sum.add_input(&inpA);
+    sum.add_input(&inpB);
+    sum.add_input(&inpC);
+    inpA.set_value(1.5);
+    inpB.set_value(2.2);
+    inpC.set_value(3.3);
+    sum.calculate();
+    ASSERT_EQ(sum.get_value(), 7.0);
+    inpA.set_value(15);
+    inpB.set_value(35.2);
+    inpC.set_value(18.8);
+    sum.calculate();
+    ASSERT_EQ(sum.get_value(), 69.0);
+}
+
+TEST(SumPoint, SumDouble)
+{
+    auto inpA = Point<double>();
+    auto inpB = Point<double>();
+    auto inpC = Point<double>();
+    auto sum = SumPoint<double>();
     sum.add_input(&inpA);
     sum.add_input(&inpB);
     sum.add_input(&inpC);
